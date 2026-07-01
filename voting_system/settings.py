49e192b4767889879ccae64c2eda6f665c60891e
Zensi-Sukhadiya <sukhadiyazensi26@gmail.com>
2026-07-01 12:15:47 +0530

    Ready for Render deployment

--- voting_system/settings.py
+++ voting_system/settings.py
@@ -77,12 +77,11 @@ WSGI_APPLICATION = 'voting_system.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",
         conn_max_age=600
-     )
+    )
 }
-
 
 
 # Redirect when not logged in
